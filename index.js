f054dce887a81bd6267b54312e2079b156602b74
b.leonardb2003 <b.leonardb2003@gmail.com>
2023-03-22 14:48:45 +0200

    Updates

--- index.js
+++ index.js
@@ -13,18 +13,19 @@ const {errorHandling} = require('./middleware/ErrorHandling');
 const cookieParser = require('cookie-parser');
 
 app.use((req, res, next)=> {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+        res.header('Access-Control-Allow-Origin', 'http://localhost:8080/')
         res.header("Access-Control-Allow-Credentials", "true")
         res.header("Access-Control-Allow-Methods", "*")
         res.header("Access-Control-Allow-Headers", "*")
         next();
 });
-app.use(route);
+
 app.use(
-    cors(),
     cookieParser(),
-    express.json,
-    express.urlencoded({extended: false})
+    cors(),
+    route,
+    express.json(),
+    express.urlencoded({extended: true})
 )
 
 // Server is running
